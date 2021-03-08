8a88178961a1b43e8ef33e5dd2c70dd11f671767
andresgIES <andres.garcia@iesonline.com.co>
2021-03-08 15:24:21 -0500

    nuevo componente

---
+++ src/app/components/loteria-semanal-nueva/loteria-semanal-nueva.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-loteria-semanal-nueva',
+  templateUrl: './loteria-semanal-nueva.component.html',
+  styleUrls: ['./loteria-semanal-nueva.component.css']
+})
+export class LoteriaSemanalNuevaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
