65560b6b882644b4a20d7925fbf8574840234ded
andresgIES <andres.garcia@iesonline.com.co>
2021-04-12 16:15:12 -0500

    reset app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,10 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
 import { AppComponent } from "./app.component";
-import { HelloComponent } from "./hello.component";
-import { JsonToComponent } from "./components/loterias-hoy/loterias-hoy";
-import { HttpClientModule } from "@angular/common/http";
-import { LoteriasSemanalesComponent } from './components/loterias-semanales/loterias-semanales.component';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, HttpClientModule],
-  declarations: [AppComponent, HelloComponent, JsonToComponent, LoteriasSemanalesComponent],
+  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
